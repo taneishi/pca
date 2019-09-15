92d80016705760e41311d2b6de210b7f900c6ec3
kei.taneishi <taneishi@users.noreply.github.com>
2019-09-15 15:52:57 +0900

    show ecfp 6 and 8

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 Principal Component Analysis
 ============================
 
-Requirements
-------------
-
-- conda install -c openbabel openbabel
+3 ways to calculate PCA
+-----------------------
 
 ![result](https://raw.githubusercontent.com/taneishi/pca/master/figure/pca.png)
+
+Plot pc1 and pc2 and molecular solubability
+-------------------------------------------
+
+- conda install -c openbabel openbabel
 
 ![result](https://raw.githubusercontent.com/taneishi/pca/master/figure/fp.png)
--- fp.py
+++ fp.py
@@ -4,6 +4,7 @@ and the influence of molecular representations on landscape topology and formati
 J Chem Inf Model, J Chem Inf Model 50, 1021-1033, 2010.
 '''
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.interpolate
 import pybel
@@ -32,43 +33,42 @@ def pca(X, npc=2):
     return pc
 
 def fp_mds(fptype):
-    fpss = []
+    fps = []
+    solubility = []
     for mol in pybel.readfile(filename='solubility.test.sdf', format='sdf'):
-        fps = mol.calcfp(fptype=fptype).bits
-        if len(fps) > 0: 
-            fpss.append(fps)
+        fp = mol.calcfp(fptype=fptype).bits
+        fps.append(fp)
+        solubility.append(np.float32(mol.data['SOL']))
 
-    cols = max(max(fps) for fps in fpss)
-    mat = np.zeros((len(fpss), cols+1), dtype=np.float32)
-
-    for i,fps in enumerate(fpss):
-        mat[i,fps] = 1.0
+    ncol = max(max(fp) if len(fp) > 0 else 0 for fp in fps)
+    mat = np.zeros((len(fps), ncol+1), dtype=np.float32)
+    for i,fp in enumerate(fps):
+        mat[i,fp] = 1.
 
     mat = scale(mat)
     pcs = np.real(pca(mat, npc=2))
-
-    activities = np.random.randn(mat.shape[0]) # dummy activity
 
     # Set up a regular grid of interpolation points
     xi, yi = np.linspace(pcs[:,0].min(), pcs[:,0].max()), np.linspace(pcs[:,1].min(), pcs[:,1].max())
     xi, yi = np.meshgrid(xi, yi)
 
     # Interpolate
-    rbf = scipy.interpolate.Rbf(pcs[:,0], pcs[:,1], activities, function='linear', smooth=0.1)
+    rbf = scipy.interpolate.Rbf(pcs[:,0], pcs[:,1], solubility, function='linear', smooth=0.1)
     zi = rbf(xi, yi)
 
-    plt.subplot(2, 2, pybel.fps[-4:].index(fptype)+1)
+    plt.subplot(2, 3, pybel.fps[-6:].index(fptype)+1)
     plt.title('%s' % fptype)
     plt.imshow(zi, vmin=zi.min(), vmax=zi.max(), origin='lower', cmap='RdYlGn_r', aspect='auto',
             extent=[pcs[:,0].min(), pcs[:,0].max(), pcs[:,1].min(), pcs[:,1].max()])
-    plt.scatter(pcs[:,0], pcs[:,1], c=activities, cmap='RdYlGn_r')
+    plt.scatter(pcs[:,0], pcs[:,1], c=solubility, cmap='RdYlGn_r')
     
 def main():
-    plt.figure(figsize=(6,6))
-    for fptype in pybel.fps[-4:]:
+    plt.figure(figsize=(9,6))
+    for fptype in pybel.fps[-6:]:
+        print(fptype)
         fp_mds(fptype)
     plt.tight_layout()
-    plt.show()
+    plt.savefig('figure/fp.png')
 
 if __name__ == '__main__':
     main()
