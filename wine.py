b034040e96121d59f30dc37adaa0a700d53611f6
taneishi <taneishi@users.noreply.github.com>
2021-04-06 12:20:25 +0900

    structured wine.py.

--- wine.py
+++ wine.py
@@ -3,14 +3,18 @@ from sklearn import datasets
 from sklearn import preprocessing
 from sklearn import decomposition
 
-dataset = datasets.load_wine()
+def main():
+    dataset = datasets.load_wine()
 
-pca = decomposition.PCA(n_components=2)
+    pca = decomposition.PCA(n_components=2)
 
-proj = pca.fit_transform(preprocessing.scale(dataset['data']))
+    proj = pca.fit_transform(preprocessing.scale(dataset['data']))
 
-plt.figure(figsize=(4,4))
-plt.scatter(proj[:,0], proj[:,1], c=dataset['target'])
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+    plt.figure(figsize=(4,4))
+    plt.scatter(proj[:,0], proj[:,1], c=dataset['target'], marker='+')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
